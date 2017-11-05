b8f066727032604d5a262c44ae073ba92fdde736
Robert Herrick <robert.herrick@outlook.com>
2017-11-04 18:18:22 -0600

    First step in moving apart API and UI

--- TimeTracker.UI/app/actions/statisticsActionConstants.js
+++
@@ -1,9 +0,0 @@
-// Task Statistics Action constants
-export const GET_TASK_STATISTICS = "GET_TASK_STATISTICS";
-export const GET_TASK_STATISTICS_SUCCESS = "GET_TASK_STATISTICS_SUCCESS";
-export const GET_TASK_STATISTICS_ERROR = "GET_TASK_STATISTICS_ERROR";
-
-// Statistics API URLs
-export const StatisticsUrls = {
-    GET_TASK_STATISTICS: ""
-};
