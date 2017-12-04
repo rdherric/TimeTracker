41be34bef12f0144523596f8e8e72d92518c18f8
Robert Herrick <robert.herrick@outlook.com>
2017-12-03 19:53:55 -0700

    Added logging to API

--- API/TimeTracker.Api/Program.cs
+++ API/TimeTracker.Api/Program.cs
@@ -1,23 +1,42 @@
 ﻿using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using NLog;
+using NLog.Web;
 
 namespace TimeTracker.Api
 {
     public class Program
     {
+        #region Constants
+        private const string _nlogConfig = "nlog.config";
+        #endregion
+
+
         public static void Main(string[] args)
         {
-            //Build the WebHost
-            IWebHost host = Program.BuildWebHost(args);
+            //Setup NLog to capture all errors
+            Logger logger = NLogBuilder.ConfigureNLog(Program._nlogConfig).GetCurrentClassLogger();
 
-            //Get that WebHost going            
-            host.Run();
+            try
+            {
+                //Build the WebHost
+                IWebHost host = Program.BuildWebHost(args);
+
+                //Get that WebHost going            
+                host.Run();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+                throw;
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseNLog()
                 .Build();
     }
 }
--- API/TimeTracker.Api/Startup.cs
+++ API/TimeTracker.Api/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NLog;
 using TimeTracker.Data.Context;
 using TimeTracker.Service;
 
@@ -11,11 +15,6 @@ namespace TimeTracker.Api
 {
     public class Startup
     {
-        #region Constants
-        public const string SqliteDatabasePathKey = "SqliteDatabasePath";
-        #endregion
-
-
         public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
@@ -26,40 +25,82 @@ namespace TimeTracker.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Setup the application in IIS
-            services.Configure<IISOptions>(opt =>
+            //Try to Configure
+            try
             {
-                opt.AutomaticAuthentication = true;
-            });
+                //Setup the application in IIS
+                services.Configure<IISOptions>(opt =>
+                {
+                    opt.AutomaticAuthentication = true;
+                });
 
-            //Add in the Database Context and services
-            services.AddDbContext<TimeTrackerContext>(opt => opt.ConfigureForTimeTracker(this.Configuration));
-            services.AddTransient<ProjectService>();
-            services.AddTransient<TaskService>();
+                //Add in the Database Context and services
+                services.AddDbContext<TimeTrackerContext>(opt => opt.ConfigureForTimeTracker(this.Configuration));
+                services.AddTransient<ProjectService>();
+                services.AddTransient<TaskService>();
 
-            //Add in the AutoMapper service
-            services.AddAutoMapper();
+                //Add in the AutoMapper service
+                services.AddAutoMapper();
 
-            //Add in MVC
-            services.AddMvc();
+                //Add in MVC
+                services.AddMvc();
+
+            }
+            catch (Exception e)
+            {
+                LogManager.GetCurrentClassLogger()?.Error(e);
+                throw;
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //Set up global logging
+            app.UseExceptionHandler(builder => builder.Run(this.GlobalLogException));
 
+            //Setup MVC for routing, etc.
             app.UseMvc();
 
             //Migrate the database
             using (IServiceScope scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
-                TimeTrackerContext ctx = scope.ServiceProvider.GetService<TimeTrackerContext>();
-                ctx.Migrate();
+                try
+                {
+                    TimeTrackerContext ctx = scope.ServiceProvider.GetService<TimeTrackerContext>();
+                    ctx.Migrate();
+
+                }
+                catch (Exception e)
+                {
+                    LogManager.GetCurrentClassLogger()?.Error(e);
+                    throw;
+                }
             }
+        }
+
+
+        /// <summary>
+        /// GlobalLogException logs any unhandled Exception to disk.
+        /// </summary>
+        /// <param name="ctx">The HttpContext of the Exception</param>
+        /// <returns>Task for async execution</returns>
+        private Task GlobalLogException(HttpContext ctx)
+        {
+            // Return a new Task
+            return Task.Factory.StartNew(() =>
+            {
+                // Get the Exception from the Context
+                Exception e = ctx.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+                // Log to disk if the Exception is found
+                if (e != null)
+                {
+                    //Get the Logger and log it
+                    Logger logger = LogManager.GetCurrentClassLogger();
+                    logger?.Error(e);
+                }
+            });
         }
     }
 }
